1a786844d5022b1c3b5eed30468b148d27c326de
XavierTep <xavierandres1@gmail.com>
2024-09-18 16:00:14 +0200

    Add files via upload

---
+++ hola.js
@@ -0,0 +1,9 @@
+
+function saludarCada5Segundos() {
+    setInterval(() => {
+      console.log("Hola mundo");
+    }, 5000); // 5000 milisegundos = 5 segundos
+  }
+  
+  // Llama a la función para iniciar el saludo periódico
+  saludarCada5Segundos(); 
